ece501cafe46ada0a22e99289e3912527691a3fe
Timur Fralik <timur11timur@gmail.com>
2024-04-01 15:26:10 +0300

    Add images

--- webpack.config.js
+++ webpack.config.js
@@ -75,6 +75,13 @@ Encore
       config: './postcss.config.js'
     }
   })
+
+  .copyFiles({
+    from: './assets/images',
+    // to: 'images/[path][name].[hash:8].[ext]',
+    to: 'images/[path][name].[ext]',
+    pattern: /\.(png|jpeg|jpg)$/
+  })
 ;
 
 module.exports = Encore.getWebpackConfig();
